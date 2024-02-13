75ca1b68a65a40eeeb0e2350db0710d7718674c1
unknown <ed.aliffadillah@gmail.com>
2024-02-13 21:43:35 +0700

    Handle Play and Pause

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
+
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
-
 import { useGetTopChartsQuery }  from "../redux/services/shazamCore";
 
+
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error} = useGetTopChartsQuery();
     const genreTitle = "Pop";
 
@@ -27,7 +32,11 @@ const Discover = () => {
                     <SongCard 
                     key={song.key}
                     song={song}
-                    i={i}/>
+                    isPlaying={isPlaying}
+                    activeSong={activeSong}
+                    data={data}
+                    i={i}
+                />
                 ))}
             </div>
         </div>
