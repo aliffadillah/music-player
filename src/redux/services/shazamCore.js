2fa6231f73c1b086912d0f6577d46d80315dea7e
unknown <ed.aliffadillah@gmail.com>
2024-02-13 20:30:01 +0700

    Add Music by World Charts

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+ export const shazamCoreAPI = createApi({
+    reducerPath: 'shazamCoreAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core7.p.rapidapi.com/charts/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'b1e755d1e3mshbfab65c2447aff4p108570jsn3e0ba1a9da0b');
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: (genres) => '/get-top-songs-in-world'}),
+    }),
+ }); 
+
+ export const {
+    useGetTopChartsQuery,
+ } = shazamCoreAPI;
