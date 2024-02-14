f1cc70b13e0ed32e20544802130c8220ffdff18c
unknown <ed.aliffadillah@gmail.com>
2024-02-14 22:47:04 +0700

    Top Artists

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -62,11 +62,30 @@ const TopPlay = () => {
 
         <div className="w-full flex flex-col mt-8">
           <div className="flex flex-row justify-between items-center">
-            <h2 className="text-white font-bold text-2xl">Top Charts</h2>
-              <Link to="/top-charts">
+            <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+              <Link to="/top-artists">
             <p className="text-gray-300 text-base cursor-pointer">see more</p>
           </Link>
           </div>
+
+          <Swiper slidePerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4">
+            {topPlays?.map((song, i) => (
+              <SwiperSlide
+              key={`song?.key`}
+              style={{ width: '25%', height: 'auto'}}
+              className="shadow-lg rounded-full animate-slideright">
+                <Link to={`/artists/${song?.artists[0].adamid}`}>
+                  <img src={song?.images.background} alt="" />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>      
       </div>
     </div>
